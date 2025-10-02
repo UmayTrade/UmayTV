d231349ec505b48b0464bd35a47759d3852a7188
UmayTrade <kocatepe828@gmail.com>
2025-10-02 14:12:32 +0300

    Update BelgeselXPlugin.kt

--- BelgeselX/src/main/kotlin/com/keyiflerolsun/BelgeselXPlugin.kt
+++ BelgeselX/src/main/kotlin/com/keyiflerolsun/BelgeselXPlugin.kt
@@ -1,15 +1,27 @@
-package com.keyiflerolsun
+package com.example.belgeselx
 
-import com.lagradost.cloudstream3.Plugin
-import com.lagradost.cloudstream3.registerMainAPI
-import com.lagradost.cloudstream3.registerExtractorAPI
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
 
 class BelgeselXPlugin : Plugin() {
-    override fun load() {
-        // Burada API’ni kaydet
+    override fun load(context: Context) {
         registerMainAPI(BelgeselXProvider())
+    }
+}
 
-        // Eğer özel extractor kullanıyorsan
-        // registerExtractorAPI(MyExtractor())
+class BelgeselXProvider : MainAPI() {
+    override var mainUrl = "https://belgeselx.com"
+    override var name = "BelgeselX"
+    override val supportedTypes = setOf(TvType.Movie)
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        return listOf(
+            MovieSearchResponse(
+                "Deneme Belgesel",
+                "$mainUrl/deneme",
+                TvType.Movie,
+                false
+            )
+        )
     }
 }
