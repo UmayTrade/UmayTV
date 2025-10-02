88d743141430aba94cb3adc005a8fc99ac222f2c
UmayTrade <kocatepe828@gmail.com>
2025-10-03 00:55:21 +0300

    Update SuperFilmGeldiPlugin.kt

--- SuperFilmGeldi/src/main/kotlin/com/keyiflerolsun/SuperFilmGeldiPlugin.kt
+++ SuperFilmGeldi/src/main/kotlin/com/keyiflerolsun/SuperFilmGeldiPlugin.kt
@@ -1,14 +1,16 @@
+// SUPERFILMGELDIPlugin.kt
+
 package com.keyiflerolsun
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.plugins.Plugin // << Artık bulunabilir olmalı
 import android.content.Context
 
 @CloudstreamPlugin
-class SuperFilmGeldiPlugin: Plugin() {
+class SuperFilmGeldiPlugin: Plugin() { // << Artık çalışmalı
     override fun load(context: Context) {
-        registerMainAPI(SuperFilmGeldi())
-        registerExtractorAPI(MixPlayHD())
-        registerExtractorAPI(MixTiger())
+        registerMainAPI(SuperFilmGeldi()) // << Artık çalışmalı
+        registerExtractorAPI(BirExtractor())
+        registerExtractorAPI(IkinciExtractor())
     }
 }
