04e4a1c6926095e4d837416c4a05777189ef90d2
UmayTrade <kocatepe828@gmail.com>
2025-10-02 23:11:18 +0300

    Update BelgeselXPlugin.kt

--- BelgeselX/src/main/kotlin/com/umaytrade/BelgeselXPlugin.kt
+++ BelgeselX/src/main/kotlin/com/umaytrade/BelgeselXPlugin.kt
@@ -1,18 +1,20 @@
-package com.umaytrade
+package com.umaytrade.belgeselx
 
+import android.content.Context
 import com.lagradost.cloudstream3.Plugin
 import com.lagradost.cloudstream3.MainAPI
-import android.content.Context
+import com.lagradost.cloudstream3.utils.AppUtils.registerMainAPI
+import com.lagradost.cloudstream3.annotations.CloudstreamPlugin
 
 @CloudstreamPlugin
-class BelgeselXPlugin: Plugin() {
+class BelgeselXPlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(BelgeselX())
     }
 }
 
 class BelgeselX : MainAPI() {
-    override var mainUrl = "https://belgeselx.com/"
+    override var mainUrl = "https://ornek-belgesel-site.com"
     override var name = "BelgeselX"
     override var lang = "tr"
 }
