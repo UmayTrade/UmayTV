68695c02f03a5a1e759580f6fd649beeb8d3200a
UmayTrade <kocatepe828@gmail.com>
2025-10-02 19:10:07 +0300

    Add files via upload

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,19 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
+rootProject.name = "CloudstreamPlugins"
+
+// This file sets what projects are included.
+// All new projects should get automatically included unless specified in the "disabled" variable.
+
+val disabled = listOf<String>()
+
+File(rootDir, ".").eachDir { dir ->
+    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
+        include(dir.name)
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+fun File.eachDir(block: (File) -> Unit) {
+    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
 }
 
-rootProject.name = "UmayTV"
-include(":app")
+// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
+// include("PluginName")
