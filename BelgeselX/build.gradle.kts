ee1aaee7d8e01d929d4350f5435c80c96fe8a306
UmayTrade <kocatepe828@gmail.com>
2025-10-24 13:24:38 +0300

    Update build.gradle.kts for Android library configuration

--- BelgeselX/build.gradle.kts
+++ BelgeselX/build.gradle.kts
@@ -1,15 +1,33 @@
-version = 1
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+android {
+    compileSdk = 34
+    namespace = "com.umaytv.belgeselx"
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
 
 cloudstream {
-    authors     = listOf("UmayTrade")
-    language    = "tr"
+    authors = listOf("UmayTrade")
+    language = "tr"
     description = "En yeni belgeseller, türkçe altyazılı yada dublaj olarak 1080p kalitesinde HD belgesel izle."
-    status  = 1
+    status = 1
     tvTypes = listOf("Documentary")
     iconUrl = "https://www.google.com/s2/favicons?domain=belgeselx.com&sz=%size%"
 }
 
-// Bağımlılık kaldırıldı - CloudStream otomatik olarak sağlıyor
-// dependencies {
-//     implementation(project(":"))
-// }
+dependencies {
+    implementation("com.lagradost:cloudstream3:pre-release")
+}
