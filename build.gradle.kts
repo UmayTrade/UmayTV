0730f6965dcfe96234da6ebbfacf968fcce570a5
UmayTrade <kocatepe828@gmail.com>
2025-10-24 13:24:15 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,7 @@
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-import com.android.build.gradle.BaseExtension
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2")
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
-    }
+plugins {
+    id("com.android.library") apply false
+    id("kotlin-android") apply false
+    id("com.lagradost.cloudstream3.gradle") apply false
 }
 
 allprojects {
@@ -20,59 +9,6 @@ allprojects {
         google()
         mavenCentral()
         maven("https://jitpack.io")
-    }
-}
-
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
-
-fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
-
-subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-    cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/UmayTrade/UmayTv")
-        authors = listOf("UmayTrade")
-    }
-
-    android {
-        compileSdkVersion(34)
-        namespace = "com.umaytv.${project.name.toLowerCase()}"
-
-        defaultConfig {
-            minSdk = 21
-            targetSdk = 34
-        }
-
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-        }
-
-        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-            kotlinOptions {
-                jvmTarget = "1.8"
-                freeCompilerArgs = freeCompilerArgs + listOf(
-                    "-Xno-call-assertions",
-                    "-Xno-param-assertions", 
-                    "-Xno-receiver-assertions"
-                )
-            }
-        }
-    }
-
-    dependencies {
-        val cloudstream by configurations
-        val implementation by configurations
-
-        cloudstream("com.lagradost:cloudstream3:pre-release")
-        implementation(kotlin("stdlib"))
-        
-        // CloudStream'in built-in dependency'leri yeterli
-        implementation("org.jsoup:jsoup:1.15.3")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     }
 }
 
