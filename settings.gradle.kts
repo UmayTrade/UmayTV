19e8591ed4df235b2a275e3901dd07030cfab007
UmayTrade <kocatepe828@gmail.com>
2025-10-24 13:18:38 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,9 @@ File(rootDir, ".").listFiles()?.filter {
     it.isDirectory && 
     File(it, "build.gradle.kts").exists() && 
     it.name != "gradle" &&
-    !it.name.startsWith(".")
+    !it.name.startsWith(".") &&
+    it.name != "build"
 }?.forEach {
     include(it.name)
+    println("Included project: ${it.name}")
 }
